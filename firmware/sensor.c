c1048e5492cf6b72327de5fe8a3fdb5e8cd5a42c
David R. Piegdon <dgit@piegdon.de>
2017-06-13 00:35:24 +0000

    use blocking version of software uart which is soooo much smaller

--- firmware/sensor.c
+++ firmware/sensor.c
@@ -4,58 +4,12 @@
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
 
-// Software UART transmitting at 1200 BAUD 8E2 (8 bits even parity 2 stop bits)
-#define UART_BUF_SIZE	4
-static char uart_buffer[UART_BUF_SIZE];
-static uint8_t uart_current = 0;
-enum {
-	UART_TX_IDLE		= 0,
-	UART_TX_PREPARE		= 1,
-	UART_TX_START_BIT	= 2,
-	UART_TX_BIT0		= 3,
-	UART_TX_BIT1		= 4,
-	UART_TX_BIT2		= 5,
-	UART_TX_BIT3		= 6,
-	UART_TX_BIT4		= 7,
-	UART_TX_BIT5		= 8,
-	UART_TX_BIT6		= 9,
-	UART_TX_BIT7		= 10,
-	UART_TX_PARITY		= 11,
-	UART_TX_STOP1		= 12,
-	UART_TX_STOP2		= 13,
-	UART_TX_TAIL1		= 14,
-	UART_TX_TAIL2		= 15,
-	UART_TX_TAIL3		= 16,
-	UART_TX_TAIL4		= 17,
-} uart_tx_state = UART_TX_IDLE;
-static uint8_t uart_end = 0;
-#define UART_IS_EMPTY()		(uart_current == uart_end)
-#define UART_IS_FULL()		(((uart_current + 1) % UART_BUF_SIZE) == uart_end)
-/* XXX NB the baud-rate may require slight tuning
- * due to internal oscillator precision.
- * start with 1200 when re-tuning for new hardware.
- * */
-#define UART_BAUD		(1220ULL)
-#define UART_BIT_TIME		((F_CPU / 1ULL) / (UART_BAUD))
-#define PIN_UART		PB0
-#define UART_LOGIC_HIGH()	PORTB &= ~(1 << PIN_UART)
-#define UART_LOGIC_LOW()	PORTB |= (1 << PIN_UART)
-
+#define PIN_UART_TX		PB0
 
 static void init_hardware(void)
 {
-	CCP = 0xD8;		// allow writes to CLKPSR
-	CLKPSR = 0b0011U;	// set prescaler to /8
-	CCP = 0xD8;		// allow writes to CLKPSR
-	CLKMSR = 0b00U;		// select internal 8MHz oscillator
-
-	// enable TIMER0
-	PRR &= ~(1 << PRTIM0);
-
 	// enable UART output pin
-	DDRB |= (1 << PIN_UART);
-	DIDR0 |= (1 << PIN_UART);
-	PUEB &= ~(1 << PIN_UART);
+	DDRB = (1 << PIN_UART_TX);
 
 	// set sleep-mode to idle so TIMER0 can still wake us up
 	SMCR = 0;
@@ -63,26 +17,56 @@ static void init_hardware(void)
 	sei();
 }
 
+#define UART_LOGIC_HIGH()	PORTB &= ~(1 << PIN_UART_TX)
+#define UART_LOGIC_LOW()	PORTB |= (1 << PIN_UART_TX)
+#define UART_BAUD		(1220ULL)
+#define UART_BIT_TIME		((F_CPU / 1ULL) / (UART_BAUD))
+
 static void uart_tx(char c)
 {
-	while(UART_IS_FULL())
-		sleep_mode(); // block until space in ringbuffer
+	char tmp;
 
-	uart_buffer[uart_end] = c;
-	uart_end = (uart_end + 1) % UART_BUF_SIZE;
+	// start transmitter timer0 (OCR0A)
+	OCR0A = UART_BIT_TIME;
+	TCCR0A = 0;
+	TCCR0C = 0;
+	TCNT0 = 0;
+	TIMSK0 = (1 << OCIE0A);// enable OCR0A interrupt
+	TCCR0B = (1 << WGM02) | 0b001;
+			// Clear timer on compare (OCR0A),
+			// TimerCLK is IoCLK
 
-	if(UART_TX_IDLE == uart_tx_state) {
-		// start transmitter timer0 (OCR0A)
-		OCR0A = UART_BIT_TIME;
-		TCNT0 = 0;
-		TCCR0A = 0;
-		TCCR0C = 0;
-		TIMSK0 = (1 << OCIE0A);// enable OCR0A interrupt
-		TCCR0B = (1 << WGM02) | 0b001;
-				// Clear timer on compare (OCR0A),
-				// TimerCLK is IoCLK
-		++uart_tx_state;
+	// start bit
+	UART_LOGIC_LOW();
+
+	// data bits
+	for(tmp = 0x01; tmp != 0; tmp <<= 1) {
+		sleep_mode();
+		if(c & tmp)
+			UART_LOGIC_HIGH();
+		else
+			UART_LOGIC_LOW();
 	}
+
+	// even parity bit
+	tmp = c;
+	tmp ^= tmp >> 4;
+	tmp ^= tmp >> 2;
+	tmp ^= tmp >> 1;
+	sleep_mode();
+	if(tmp & 1)
+		UART_LOGIC_HIGH();
+	else
+		UART_LOGIC_LOW();
+
+	// stop bit 1, stop bit 2, and 4 more tail
+	for(tmp = 6; tmp != 0; --tmp) {
+		sleep_mode();
+		UART_LOGIC_HIGH();
+	}
+
+	// disable transmitter timer0
+	TCCR0B = 0;
 }
 
 int main(void)
@@ -107,50 +91,6 @@ int main(void)
 
 ISR(TIM0_COMPA_vect)
 {
-	register uint8_t val = uart_buffer[uart_current];
-	if(!UART_IS_EMPTY()) {
-		++uart_tx_state;
-		switch(uart_tx_state) {
-			case UART_TX_START_BIT:
-				UART_LOGIC_LOW();
-				break;
-			case UART_TX_PARITY:
-				{
-					val ^= val >> 4;
-					val ^= val >> 2;
-					val ^= val >> 1;
-					if(val & 1)
-						UART_LOGIC_HIGH();
-					else
-						UART_LOGIC_LOW();
-				}
-				break;
-			case UART_TX_STOP1:
-				UART_LOGIC_HIGH();
-				break;
-			case UART_TX_TAIL1:
-				/* fall through */
-			case UART_TX_TAIL2:
-			case UART_TX_TAIL3:
-				/* fall through */
-			case UART_TX_STOP2:
-				break;
-			case UART_TX_TAIL4:
-				uart_tx_state = UART_TX_PREPARE;
-				uart_current = (uart_current + 1) % UART_BUF_SIZE;
-				break;
-			default:
-				/* transmit current bit */
-				val >>= (uart_tx_state - UART_TX_BIT0);
-				if(val & 1)
-					UART_LOGIC_HIGH();
-				else
-					UART_LOGIC_LOW();
-				break;
-		}
-	} else {
-		TCCR0B = 0; // disable transmitter timer0
-		uart_tx_state = UART_TX_IDLE;
-	}
+	return;
 }
 
